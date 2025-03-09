056cea63ee1213270067ce8d3ed55f8a0d5756d4
jcoder <fronzesvelex@gmail.com>
2025-03-09 11:57:50 +0300

    Вот и настал момент.... когда я сделал задание)

--- src/main/java/com/example/demo/controller/ControllerApplication.java
+++ src/main/java/com/example/demo/controller/ControllerApplication.java
@@ -3,11 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.model.Information;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(value = "/info/user")
 public class ControllerApplication {
 
     @GetMapping(value = "/info/user")
