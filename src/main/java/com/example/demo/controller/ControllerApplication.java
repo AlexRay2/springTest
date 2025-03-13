e7c11599774197f7ff6b53af4882b4fe2b4c1e68
jcoder <fronzesvelex@gmail.com>
2025-03-13 22:04:56 +0300

    get all

--- src/main/java/com/example/demo/controller/ControllerApplication.java
+++ src/main/java/com/example/demo/controller/ControllerApplication.java
@@ -7,21 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/info")
 public class ControllerApplication {
-
+    Data data = new Data();
+    UUID id1 = UUID.randomUUID();
+    UUID id2 = UUID.randomUUID();
+    UUID id3 = UUID.randomUUID();
     @GetMapping(value = "/user")
     public ResponseEntity<Information> getInfo() {
-        Data data = new Data();
-        UUID id1 = UUID.randomUUID();
-        UUID id2 = UUID.randomUUID();
-        UUID id3 = UUID.randomUUID();
-        data.hashMap.put(id1, Information.builder().name("Alex").age(12).build());
-        data.hashMap.put(id2, Information.builder().name("Elya").age(12).build());
-        data.hashMap.put(id3, Information.builder().name("Slava").age(12).build());
+        data.hashMap.put(id1, data.Alex);
+        data.hashMap.put(id2, data.Elya);
+        data.hashMap.put(id3, data.Slava);
         return ResponseEntity.ok().body(data.finder(id2));
+    }
+    @GetMapping(value = "/users")
+    public ResponseEntity<List> getAll(){
+        return ResponseEntity.ok().body(data.allStudents());
     }
 }
--- src/main/java/com/example/demo/database/Data.java
+++ src/main/java/com/example/demo/database/Data.java
@@ -1,13 +1,28 @@
 package com.example.demo.database;
 
 import com.example.demo.model.Information;
+import org.apache.catalina.LifecycleState;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.UUID;
 
 public class Data {
+    public Information Alex = Information.builder().name("Alex").age(12).build();
+    public Information Elya = Information.builder().name("Elya").age(12).build();
+    public Information Slava = Information.builder().name("Slava").age(15).build();
+
     public HashMap<UUID, Information> hashMap = new HashMap<>();
-    public Information finder(UUID id){
+
+    public Information finder(UUID id) {
         return hashMap.get(id);
+    }
+
+    public List<Information> allStudents() {
+        return List.of(
+                Alex,
+                Elya,
+                Slava
+        );
     }
 }
