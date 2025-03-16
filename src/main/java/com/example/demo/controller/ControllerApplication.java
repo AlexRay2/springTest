f218d6e3ccf4accc5759a1d738638bf985305b1a
jcoder <fronzesvelex@gmail.com>
2025-03-16 20:22:34 +0300

    перенес id

--- src/main/java/com/example/demo/controller/ControllerApplication.java
+++ src/main/java/com/example/demo/controller/ControllerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,10 +19,17 @@ public class ControllerApplication {
         data.hashMap.put(data.id1, data.Alex);
         data.hashMap.put(data.id2, data.Elya);
         data.hashMap.put(data.id3, data.Slava);
-        return ResponseEntity.ok().body(data.finder(data.id2));
+        UUID testUUID = UUID.randomUUID();
+        UUID search = testUUID;
+        if (!data.hashMap.containsKey(search))
+            return ResponseEntity.ok().body(Information.builder().name("Unprocessable Entity").age(422).build());
+        if (search == null)
+            return ResponseEntity.ok().body(Information.builder().name("Bad Request").age(400).build());
+        return ResponseEntity.ok().body(data.finder(search));
     }
+
     @GetMapping(value = "/users")
     public ResponseEntity<List> getAll(){
-        return ResponseEntity.ok().body(data.allStudents());
+        return ResponseEntity.ok().body(data.allStudents()); 
     }
 }
--- src/main/java/com/example/demo/database/Data.java
+++ src/main/java/com/example/demo/database/Data.java
@@ -1,7 +1,6 @@
 package com.example.demo.database;
 
 import com.example.demo.model.Information;
-import org.apache.catalina.LifecycleState;
 
 import java.util.HashMap;
 import java.util.List;
